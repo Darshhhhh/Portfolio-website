fc0c0e37a258dbca8074dfc66fd0766abc82fbb6
Darshhhhh <contact.shahdarsh@gmail.com>
2025-11-19 10:05:45 -0500

    Resume Update 2025

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Resume from "../../assets/Darsh-Shah-Resume-SDE.pdf";
+import Resume from "../../assets/Darsh-Shah-Resume-v2.pdf";
 import Header from "../Header/Header";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 
